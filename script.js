22c73ecff9aa1cffcb35aca2709430b021d0bbc2
hamzab710 <hamzabessali8@gmail.com>
2023-10-01 18:16:46 +0100

    Add a color changer and Change UI Change the "change dimensions" button to a slider to to limit the user input to numbers between 1 and 100. Change the "rainbow mod" and "eraser" buttons to checkboxes. Reorganized the script. Changed conatiner size and direction of inputs.

--- script.js
+++ script.js
@@ -1,59 +1,49 @@
 const container = document.getElementById('container');
-const restart = document.getElementById('restart');
 const clear = document.getElementById('clear');
-const dimensions = document.querySelector('.dimensions');
-const submit = document.querySelector('.submit');
-const eraser = document.querySelector('.eraser');
+const dimensions = document.getElementById('dimensions');
+const eraser = document.getElementById('eraser');
+const dimensionsLabel = document.querySelector('label[for=dimensions]');
 const rainbow = document.getElementById('rainbow');
+const colorInput = document.getElementById('colors');
 
-function clearContainer() {
-    while (container.firstChild) {
-        container.removeChild(container.firstChild);
-    }
-}
+dimensionsLabel.textContent = `${dimensions.value} X ${dimensions.value}`
+
 const numberOfSquares = 256;
 for (let i = 0; i < numberOfSquares; i++) {
         const grid = document.createElement('div');
         grid.classList.add("square");
 
         grid.addEventListener('mousedown', function () {
-            grid.style.backgroundColor = 'black';
+            grid.style.backgroundColor = `${colorInput.value}`;
             });
 
-        grid.addEventListener('mouseover', function () {
+        grid.addEventListener('mouseover', function (event) {
             if (event.buttons === 1) {
-                grid.style.backgroundColor = 'black';
+                grid.style.backgroundColor = `${colorInput.value}`;
             }
         });
 
     container.appendChild(grid);
 }
 
-clear.addEventListener('click',function () {
-    const grid = document.querySelectorAll('.square');
-    grid.forEach(function (grid) {
-        grid.style.backgroundColor = 'white'
-    })
-});
-
 function updateSquareWidth(newWidth) {
     container.style.setProperty('--square-width', newWidth);
 }
 
 function givePrompt() {
-    const userNumber = Number(dimensions.value);
+    const userNumber = dimensions.value;
     const numberOfSquares =  userNumber * userNumber;
     for (let i = 0; i < numberOfSquares; i++) {
         const grid = document.createElement('div');
         grid.classList.add("square");
 
         grid.addEventListener('mousedown', function () {
-            grid.style.backgroundColor = 'black';
+            grid.style.backgroundColor = `${colorInput.value}`;
             });
 
-        grid.addEventListener('mouseover', function () {
+        grid.addEventListener('mouseover', function (event) {
             if (event.buttons === 1) {
-                grid.style.backgroundColor = 'black';
+                grid.style.backgroundColor = `${colorInput.value}`;
             }
         });
 
@@ -63,21 +53,11 @@ function givePrompt() {
 updateSquareWidth(`${100 / Math.sqrt(numberOfSquares)}%`);
 }
 
-restart.addEventListener('click', function () {
-    const dimensions = document.querySelector('.dimensions');
-    const submit = document.querySelector('.submit');
-    dimensions.style.display = 'inline';
-    submit.style.display = 'inline';
-})
-
-submit.addEventListener('click',function () {
-    clearContainer();
-    givePrompt();
-    const dimensions = document.querySelector('.dimensions');
-    const submit = document.querySelector('.submit');
-    dimensions.style.display = 'none';
-    submit.style.display = 'none';
-});
+function clearContainer() {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
 
 function erase() {
     const grid = document.querySelectorAll('.square');
@@ -93,8 +73,6 @@ function erase() {
         });
     })
 }
-
-eraser.addEventListener('click', erase);
 
 function makeRainbow() {
     const grid = document.querySelectorAll('.square');
@@ -117,4 +95,52 @@ function makeRainbow() {
     })
 }
 
-rainbow.addEventListener('click', makeRainbow);
+function changeColor() {
+    const grid = document.querySelectorAll('.square');
+    grid.forEach(function(grid) {
+        grid.addEventListener('mousedown', function () {
+            grid.style.backgroundColor = `${colorInput.value}`;
+            });
+        grid.addEventListener('mouseover', function (event) {
+            if (event.buttons === 1) {
+                grid.style.backgroundColor = `${colorInput.value}`;
+            }
+        });
+    });
+}
+
+dimensions.addEventListener('input',function(event) {
+    clearContainer();
+    givePrompt();
+    dimensionsLabel.textContent = `${dimensions.value} X ${dimensions.value}`
+});
+
+clear.addEventListener('click',function () {
+    const grid = document.querySelectorAll('.square');
+    grid.forEach(function (grid) {
+        grid.style.backgroundColor = 'white'
+    })
+});
+
+eraser.addEventListener('change', () => {
+    if (eraser.checked) {
+        erase();
+        if (rainbow.checked) {
+            rainbow.checked = false
+        }
+    } else {
+        changeColor();
+    }
+});
+
+rainbow.addEventListener('change', () => {
+    if (rainbow.checked) {
+        makeRainbow();
+        if (eraser.checked) {
+            eraser.checked = false
+        }
+    } else {
+        changeColor();
+    }
+});
+
